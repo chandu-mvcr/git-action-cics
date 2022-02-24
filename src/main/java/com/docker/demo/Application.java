8de70d8dce7ad85d0932e32e2c6349d78e1eb20e
chandu-mvcr <maramvenkatachandra@gmail.com>
2022-02-24 10:26:14 +0530

    add project

---
+++ src/main/java/com/docker/demo/Application.java
@@ -0,0 +1,28 @@
+package com.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@GetMapping("/message")
+	public String getmessage(){
+		return "Welcome to Docker demo....!!";
+	}
+
+	/*echo "# git-action-cics" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/chandu-mvcr/git-action-cics.git
+	git push -u origin main*/
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
